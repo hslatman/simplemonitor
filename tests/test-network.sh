9ad6c12b20d59f3e0f1befc57521d5965979a003
James Seward <james@jamesoff.net>
2018-09-11 16:36:31 +0100

    Allow networking test script to operate without coverage

--- tests/test-network.sh
+++ tests/test-network.sh
@@ -4,19 +4,27 @@ set -ex
 
 rm -f network.log
 
+if [[ $WITHOUT_COVERAGE -eq 1 ]]; then
+	my_command="python"
+else
+	my_command="coverage run --debug=dataio"
+fi
+
 # start the master instance
-COVERAGE_FILE=.coverage.1 coverage run --debug=dataio monitor.py -f tests/network/master/monitor.ini -d --loops=2 &
+COVERAGE_FILE=.coverage.1 $my_command monitor.py -f tests/network/master/monitor.ini -d --loops=2 &
 sleep 1
 
 # run the client instance
-COVERAGE_FILE=.coverage.2 coverage run --debug=dataio monitor.py -f tests/network/client/monitor.ini -1 -d
+COVERAGE_FILE=.coverage.2 $my_command monitor.py -f tests/network/client/monitor.ini -1 -d
 
 # let them run
 sleep 15
 
-# make sure the client reached the master 
+# make sure the client reached the master
 grep test2 network.log
 
 wait
 
-coverage combine --append
+if [[ $WITHOUT_COVERAGE -ne 1 ]]; then
+	coverage combine --append
+fi
